f4bc3ec8a3898508c9f2fcc64f507f224c5c715e
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-01-15 19:32:55 +0100

    asd

--- src/Gui/Menu.java
+++ src/Gui/Menu.java
@@ -1,15 +1,63 @@
 package Gui;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.WindowConstants;
+
+import chat.ResourceLoader;
 
 public class Menu {
 	
-	private JFrame Mainframe;
+	public JFrame Mainframe;
+	public JPanel Mainpanel;
+	public JLabel Mainlabel;
+
+	
+
+	Dimension minimumSize = new Dimension(100, 50);
 	
 	public Menu(){
 		
-		JFrame Mainframe = new JFrame();
-		Mainframe.setSize(800,600);
+		/////////////////////////////IMAGES//////////////////////////////////////////
+		
+		BufferedImage mainpanel_bf = null;
+		
+		try {
+			mainpanel_bf = ImageIO.read(ResourceLoader.load("/Mainpanel.png"));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		///////////////////////////MAIN COMPONENTS///////////////////////////////////
+		
+		JFrame Mainframe = new JFrame("WeChat");
+		Mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Mainframe.setSize(1200, 800);
+		Mainframe.setLocationRelativeTo(null);
+		Mainframe.setResizable(false);
+		Mainframe.setVisible(true);
+		
+		JPanel Mainpanel = new JPanel();
+		Mainpanel.setBounds(0,20,1200,800);
+		
+		JLabel Mainlabel = new JLabel(new ImageIcon(mainpanel_bf));
+		Mainlabel.setBounds(0,0,1200,800);
+
+		////////////////////////////OTHER COMPONENTS////////////////////////////////////
+
+		
+		
+		Mainframe.add(Mainpanel);
+		Mainpanel.add(Mainlabel);
+		
 
 	}
 	public static void main(String[] args) {
