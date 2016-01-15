a76b2fbaa824f0dc866f792205d6d806127aa35d
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-01-15 18:17:39 +0100

    adsa

---
+++ src/Gui/Menu.java
@@ -0,0 +1,18 @@
+package Gui;
+
+import javax.swing.JFrame;
+
+public class Menu {
+	
+	private JFrame Mainframe;
+	
+	public Menu(){
+		
+		JFrame Mainframe = new JFrame();
+		Mainframe.setSize(800,600);
+
+	}
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+	}
+}
