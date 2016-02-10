177a35cf83121069331f129140ccd381b592cb7e
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-02-10 17:45:03 +0100

    asdas

--- src/Gui/Menu.java
+++ src/Gui/Menu.java
@@ -2,6 +2,10 @@ package Gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -23,19 +27,22 @@ public class Menu {
 	public JFrame friendlistframe;
 	public JPanel friendlistpanel;
 	public JLabel friendlistlabel;
-
+	
+	int a,b = 0;
+	int atemp, btemp = 0;
+	
 	Color yellow = new Color(241, 196, 15);
-	
-	private int x;
-	private int y;
-	
+
 	public Menu(){
 		
 		/////////////////////////////IMAGES//////////////////////////////////////////
 
 		BufferedImage mainpanel_bf = null;
+		BufferedImage friendlistpanel_bf = null;
+		
 		try {
 			mainpanel_bf = ImageIO.read(ResourceLoader.load("/Mainpanel.png"));
+			friendlistpanel_bf = ImageIO.read(ResourceLoader.load("/friendlist.png"));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -44,23 +51,26 @@ public class Menu {
 		
 		JFrame mainframe = new JFrame("WeChat");
 		mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		mainframe.setBounds(x,y,1000, 800);
+		mainframe.setBounds(0,0,1000, 800);
 		mainframe.setLocationRelativeTo(null);
 		mainframe.setResizable(false);
 		mainframe.setVisible(true);
 		
+		a = mainframe.getX();
+		b = mainframe.getY();
+		
 		JPanel mainpanel = new JPanel();
 		mainpanel.setBounds(0,0,1200,800);
 		mainpanel.setBackground(yellow);
-		
+
 		JLabel mainlabel = new JLabel(new ImageIcon(mainpanel_bf));
 		mainlabel.setBounds(0,0,1200,800);
+		
 
 		////////////////////////////OTHER COMPONENTS////////////////////////////////////
 
-		JFrame friendlistframe = new JFrame("Friends");
-		friendlistframe.setBounds(0,0,300, 800);
-		friendlistframe.setLocationRelativeTo(null);
+		JFrame friendlistframe = new JFrame("Freunde");
+		friendlistframe.setBounds(a+995,b,300, 800);
 		friendlistframe.setResizable(false);
 		friendlistframe.setVisible(true);
 		
@@ -68,12 +78,28 @@ public class Menu {
 		friendlistpanel.setBounds(0,0,200,800);
 		friendlistpanel.setBackground(yellow);
 		
-		JLabel friendlistlabel = new JLabel(new ImageIcon(mainpanel_bf));
+		JLabel friendlistlabel = new JLabel(new ImageIcon(friendlistpanel_bf));
 		friendlistlabel.setBounds(0,0,200,800);
+		
 		
 		mainframe.add(mainpanel);
 		mainpanel.add(mainlabel);
+		friendlistframe.add(friendlistpanel);
+		friendlistpanel.add(friendlistlabel);
 		mainframe.repaint();
+		
+		
+		
+		while(true){
+			atemp = mainframe.getX();
+			btemp = mainframe.getY();
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+			}
+			if(a != atemp)friendlistframe.setBounds(atemp+995, btemp, 300, 800);
+			if(b != btemp)friendlistframe.setBounds(atemp+995, btemp, 300, 800);
+		}
 
 	}
 	public static void main(String[] args) {
